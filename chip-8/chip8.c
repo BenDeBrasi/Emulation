e0b4f26954dfd81ced98ccd576721aac79ba6453
root <root@DESKTOP-J803JUI.home>
2017-01-07 12:03:41 -0500

    First commit

---
+++ chip-8/chip8.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	
+	//current opcode
+	unsigned short curr_opcode;
+	
+	//System memory
+	unsigned char memory[4096];
+	
+	//CPU registers
+	unsigned char V[16];
+	
+	//Index/address register
+	unsigned short I;
+
+	//Program counter
+	unsigned short pc;
+
+	//pixel state array 1 if lighted 0 if dark
+	unsigned char screen[64 *32];
+
+	//Timers 1 sound 1 gameplay
+	unsigned char delay_timer;
+	unsigned char sound_timer;
+
+	
+}
